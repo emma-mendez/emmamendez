d3d3772684255a1163c80531e222fefb87b1abfb
Emma Mendez <emmamendez07@gmail.com>
2021-02-04 15:05:33 +0000

    first commit

---
+++ Desktop/react-course-template/src/components/Confirm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText } from '@material-ui/core/';
+import Button from '@material-ui/core/Button';
+
+export class Confirm extends Component {
+  continue = e => {
+    e.preventDefault();
+    // PROCESS FORM //
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const {
+      values: { firstName, 
+        lastName, 
+        country, 
+        contactNumber, 
+        email,
+        socialFB,
+        socialIG,
+        needs,
+        bio  }
+    } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Confirm User Data" />
+            <List>
+              <ListItem>
+                <ListItemText primary="First Name" secondary={firstName} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Last Name" secondary={lastName} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Contact Number" secondary={contactNumber} />
+                </ListItem>
+                <ListItem>
+                <ListItemText primary="Email" secondary={email} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Social 1" secondary={socialFB} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Social 2" secondary={socialIG} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Needs" secondary={needs} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Bio" secondary={bio} />
+              </ListItem>
+            </List>
+            <br />
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.back}
+            >Back</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Confirm & Continue</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Confirm;
---
+++ Desktop/react-course-template/src/components/FormPersonalDetails.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export class FormPersonalDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Enter Personal Details" />
+            <TextField
+              placeholder="Enter Your Country"
+              label="Country"
+              onChange={handleChange('Country')}
+              defaultValue={values.country}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Anything you`d like me to know? &#10;
+              Breathing/&#10;
+              recording/&#10;
+              upcoming performance?"
+              label="Needs"
+              onChange={handleChange('needs')}
+              defaultValue={values.needs}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Bio"
+              label="Bio"
+              onChange={handleChange('bio')}
+              defaultValue={values.bio}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.back}
+            >Back</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Continue</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default FormPersonalDetails;
